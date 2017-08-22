3d01377ece4ecfdad4c9243e1e1273299e61868e
helllth <th@cismet.de>
2017-08-22 13:27:02 +0200

    working on #2: added another zip test

--- test/Zip.js
+++ test/Zip.js
@@ -72,7 +72,62 @@ describe('ZIP Tests', () => {
                     }).catch((error) => {
                         done(error);
                     });
+                });
+        });
+    });
 
+
+    describe('POST /api/zip/and/wait', () => {
+        it('it should download and zip the given pdfs. The result should be in the right structure (multiple files in one folder)', (done) => {
+            let port = server.address().port;
+            let conf = {
+                "name": "conf01",
+                "files": [{
+                        "uri": "http://localhost:" + port + "/testresources/1.pdf",
+                        "folder": "a"
+                    },
+                    {
+                        "uri": "http://localhost:" + port + "/testresources/2.pdf",
+                        "folder": "b"
+                    },
+                    {
+                        "uri": "http://localhost:" + port + "/testresources/3.pdf",
+                        "folder": "z"
+                    },
+                    {
+                        "uri": "http://localhost:" + port + "/testresources/4.pdf",
+                        "folder": "z"
+                    },
+                    {
+                        "uri": "http://localhost:" + port + "/testresources/5.pdf",
+                        "folder": "z"
+                    },
+                ]
+            }
+            chai.request(server)
+                .post('/api/zip/and/wait')
+                .send(conf)
+                .buffer()
+                .parse(binaryParser)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    JSZip.loadAsync(res.body).then(function (zip) {
+                        zip.should.have.property('files');
+                        zip.files.should.have.property('a/');
+                        zip.files['a/'].dir.should.be.true;
+                        zip.files.should.have.property('a/1.pdf');
+                        zip.files.should.have.property('b/');
+                        zip.files['b/'].dir.should.be.true;
+                        zip.files.should.have.property('b/2.pdf');
+                        zip.files.should.have.property('z/');
+                        zip.files['z/'].dir.should.be.true;
+                        zip.files.should.have.property('z/3.pdf');
+                        zip.files.should.have.property('z/4.pdf');
+                        zip.files.should.have.property('z/5.pdf');
+                        done();
+                    }).catch((error) => {
+                        done(error);
+                    });
                 });
         });
     });
