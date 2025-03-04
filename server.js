4fed612f459118573498bb1305592376e34dc300
Thorsten Hell <thorsten.hell@cismet.de>
2025-03-04 18:57:16 +0100

    improve fork me on github

--- server.js
+++ server.js
@@ -33,14 +33,14 @@ var defaults = {
     "targetWhitelist": "",
     "corsAccessControlAllowOrigins": ['http://localhost:*'],
     "serverSourceEncoding": 'UTF-8',
-    "allowedEncodings": ['CP850','UTF-8'],
+    "allowedEncodings": ['CP850', 'UTF-8'],
     "failFast": true
 };
 
 var conf = {
     "port": extConf.port || defaults.port,
     "host": extConf.host || defaults.host,
-    "server":  extConf.server || defaults.server,    
+    "server": extConf.server || defaults.server,
     "workers": extConf.workers || defaults.workers,
     "tmpFolder": extConf.tmpFolder || defaults.tmpFolder,
     "keepFilesForDebugging": extConf.keepFilesForDebugging || defaults.keepFilesForDebugging,
@@ -52,8 +52,8 @@ var conf = {
     "failFast": extConf.failFast
 };
 
-if (conf.failFast===undefined){
-    conf.failFast=defaults.failFast;
+if (conf.failFast === undefined) {
+    conf.failFast = defaults.failFast;
 }
 
 debug("CONFIG:");
@@ -68,14 +68,30 @@ function log(message, nonce) {
     fs.appendFile(conf.tmpFolder + "processing_of_" + nonce + ".log", message + '\n');
 }
 
-
 function respondWithHelloWorld(req, res, next) {
     var body = '<html style="font-family: sans-serif;text-align:center"><link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">' +
+        '<style>' +
+        '.github-corner { position: absolute; top: 0; right: 0; }' +
+        '.github-corner:hover .octo-arm { animation: octocat-wave 560ms ease-in-out; }' +
+        '@keyframes octocat-wave {' +
+        '0%, 100% { transform: rotate(0); }' +
+        '20%, 60% { transform: rotate(-25deg); }' +
+        '40%, 80% { transform: rotate(10deg); }' +
+        '}' +
+        '@media (max-width: 500px) {' +
+        '.github-corner:hover .octo-arm { animation: none; }' +
+        '.github-corner .octo-arm { animation: octocat-wave 560ms ease-in-out; }' +
+        '}' +
+        '</style>' +
         '<h1>Dr. Processor is here with it\'s API</h1><img src="https://user-images.githubusercontent.com/837211/29552552-25ad0ec8-8718-11e7-8020-b1d85c12c872.png"/>' +
-        '<a href="https://github.com/cismet/doc-processor">' +
-        '<img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png">' +
+        '<a href="https://github.com/cismet/doc-processor" class="github-corner" aria-label="View source on GitHub">' +
+        '<svg width="80" height="80" viewBox="0 0 250 250" style="fill:#6d6d6d; color:#fff;" aria-hidden="true">' +
+        '<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>' +
+        '<path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>' +
+        '<path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>' +
+        '</svg>' +
         '</a>' +
-        '<div style="position: absolute; bottom: 5px;"><h4>done with <i class="em em-heart"></i> from <a href="https://cismet.de/en">cismet.de</a></h4  ></div>' +
+        '<div style="position: absolute; bottom: 5px;"><h4>done with <i class="em em-heart"></i> from <a href="https://cismet.de/en">cismet.de</a></h4></div>' +
         '</html>';
     res.writeHead(200, {
         'Content-Length': Buffer.byteLength(body),
@@ -83,6 +99,7 @@ function respondWithHelloWorld(req, res, next) {
     });
     res.write(body);
     res.end();
+    return next();
 }
 
 function respondForGETProcessAndWaitForX(req, res, next) {
@@ -122,18 +139,18 @@ function respondForPOSTProcessAndWait(what, req, res, next) {
 }
 
 function respondForGETDownloadResult(req, res, next) {
-        if (req.params.processor !== undefined &&
-            processors[req.params.processor] !== undefined && 
-            typeof processors[req.params.processor].download === 'function' &&
-            req.params.hash !== undefined &&
-            req.params.dlname !== undefined
-        ) {
-            processors[req.params.processor].download(req.params.hash,req.params.dlname,conf,res, next);
-        }
-        else {
-            res.send(400, 'Your request is confusing. Please check');
+    if (req.params.processor !== undefined &&
+        processors[req.params.processor] !== undefined &&
+        typeof processors[req.params.processor].download === 'function' &&
+        req.params.hash !== undefined &&
+        req.params.dlname !== undefined
+    ) {
+        processors[req.params.processor].download(req.params.hash, req.params.dlname, conf, res, next);
+    }
+    else {
+        res.send(400, 'Your request is confusing. Please check');
 
-        }
+    }
 }
 
 function requestTest(req, jsonBody) {
@@ -183,9 +200,9 @@ var server = restify.createServer();
 //server.use(restify.queryParser());
 server.use(restifyBodyParser());
 const cors = corsMiddleware({
-  origins: conf.corsAccessControlAllowOrigins,
+    origins: conf.corsAccessControlAllowOrigins,
 })
- 
+
 server.pre(cors.preflight)
 server.use(cors.actual)
 
@@ -207,6 +224,6 @@ if (process.env.NODE_ENV === 'test') {
         directory: __dirname,
         default: '/index.html'
     }));
-    server.conf.deleteFilesEvenOnErrors=true;
+    server.conf.deleteFilesEvenOnErrors = true;
 }
 module.exports = server;
